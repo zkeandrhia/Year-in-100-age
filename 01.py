eebea4a307b1aaf5740199975bdfb8b228270d98
zke <85144998+zkeandrhia@users.noreply.github.com>
2021-07-16 17:51:48 +0800

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,8 @@
+name = str(input(('Enter your name: ')))
+age = int(input('Enter your age: '))
+
+#year that will turn 100
+year = str((2021 - age)+100)
+
+print(f'Hi {name}, you are {age} today. You will turn 100 by the year {year}')
+    
